e3a53a6c460e5665bbf33105b67f6a071e8d0f15
fxsb <56769754+fxsb@users.noreply.github.com>
2019-10-19 15:00:32 -0400

    Add files via upload

---
+++ text_adventure.py
@@ -0,0 +1,31 @@
+print("Hello!")
+
+answer = input("Do you want to go on an adventure (type yes or no)? ")
+if answer == "yes":
+  print("Once upon a time...")
+  #continue your story here using print and input statements
+  print("In a castle there was a wizard who loved doing magic tricks so he came up to you to ask for a name")
+  name=input("What is your name (type your name)?")
+  print("hello", name, "so we will get started in the basement you walk in the basement and see a big hole in the roof and lot's of webs and a raccoons nest and birds in the basement the wizard say's subushka water to you and waves his wand at you turning you into a bucket of water ")
+  answer=input("do you want to end the story (type yes or no)?")
+  if answer=="no":
+    print("he puts you in a bucket and puts his wand in the bucket to with you and then say's make yourself back to human again warning don't turn yourself into a fruit")
+    answer=input("you move the wand left or right and say's subushka (type human left or human right)")
+    if answer=="human right":
+      print("you transform back into a human and lands on you behind")
+    elif answer=="human left":
+      fruit=input("what fruit do you want to turn into(name of fruit)")
+      print(name, "you turned to a",fruit,"then the racoon looks at you and follows you because he wants to eat you and you start running there was to hiding place the trash can or jump out the hole in the roof")
+  
+
+
+
+
+
+
+
+
+else:
+  print("Ok")
+  
+print("The end")
